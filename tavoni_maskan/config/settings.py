3123c44b4be71df127fd291be9088d6366f4ffe4
mohammad <m.roohollahnejad@chmal.ir>
2025-11-14 12:50:07 +0330

    Initial commit

--- tavoni_maskan/config/settings.py
+++ tavoni_maskan/config/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-key')
 
 DEBUG = False
-ALLOWED_HOSTS = ['https://tavoni_maskan.onrender.com']  # در حالت توسعه خالی می‌ماند
+ALLOWED_HOSTS = ['*']  # در حالت توسعه خالی می‌ماند
 
 # ==============================
 # اپلیکیشن‌ها
